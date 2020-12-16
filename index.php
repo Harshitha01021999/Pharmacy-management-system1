5f835be3bbfc65b446815ca807f44d03ac9c759b
Harshitha01021999 <66356783+Harshitha01021999@users.noreply.github.com>
2020-12-16 17:00:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <?php include_once 'includes/head.php'; ?>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Home</title>
+  </head>
+  <body>
+    <?php include_once 'includes/nav.php'; ?>
+
+     <div class="img">
+      <img id="bg" src="images/3.jpg" style="height: 100vh; width: 100%;">
+    </div>
+    <div class="title" style="position: abs">
+        <h1>PHARMACY <br> MANAGEMENT <br> SYSTEM</h1>
+    </div>
+    <div id="about">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+     </div> 
+    <?php include_once 'includes/footer.php'; ?> 
+  </body>
+</html>
